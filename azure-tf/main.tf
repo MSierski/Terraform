771b91c14a5805845b4afccb60ba29f1e15971f1
MSierski <92363171+MSierski@users.noreply.github.com>
2023-04-28 22:11:07 +0200

    Update main.tf

--- azure-tf/main.tf
+++ azure-tf/main.tf
@@ -3,20 +3,20 @@ provider "azurerm" {
 }
 
 # Generate a random integer to create a globally unique name
-resource "random_integer" "ri" {
-  min = 10000
-  max = 99999
-}
+#resource "random_integer" "ri" {
+#  min = 10000
+#  max = 99999
+#}
 
 # Create the resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup-${random_integer.ri.result}"
+  name     = "myResourceGroup-69953"
   location = "eastus"
 }
 
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
-  name                = "webapp-asp-${random_integer.ri.result}"
+  name                = "webapp-asp-69953"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
@@ -25,7 +25,7 @@ resource "azurerm_service_plan" "appserviceplan" {
 
 # Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "webapp" {
-  name                  = "webapp-${random_integer.ri.result}"
+  name                  = "webapp-69953"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   service_plan_id       = azurerm_service_plan.appserviceplan.id
